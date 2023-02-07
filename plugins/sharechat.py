f1731abb85bc201af8d0286ba2616cc21c0a2bf4
ParvathiRNair <124461471+ParvathiRNair@users.noreply.github.com>
2023-02-07 07:05:27 +0530

    Update sharechat.py

--- plugins/sharechat.py
+++ plugins/sharechat.py
@@ -8,3 +8,11 @@ import os
 @Client.on_message(filters.text)
 async def share(bot, cmd):
 	print(cmd)
+        if "sharechat.com" in cmd.text:
+        url = cmd.text;
+        rep = url.replace("post","video")
+        response = requests.get(rep)
+        video_url = re.search(r'property="og:video" content="(.*?)"', response.text).group(1)
+        print(video_url);
+        video_link = video_url
+        bot.send_video(chat_id=message.chat.id, video=video_url,caption="Thank You For Using Me :)")
