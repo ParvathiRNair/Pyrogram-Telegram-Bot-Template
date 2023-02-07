dfe0f89854afec01e20f52baa630c71c9991f384
ParvathiRNair <124461471+ParvathiRNair@users.noreply.github.com>
2023-02-07 08:04:30 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -67,7 +67,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=START_TXT,
+            text=START_TXT.format(cmd.from_user.first_name),
             reply_markup=reply_markup,
             disable_web_page_preview=True
         )
