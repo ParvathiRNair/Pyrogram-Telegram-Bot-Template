c531c49666a513cf3837e0676c205fc67997aa93
ParvathiRNair <124461471+ParvathiRNair@users.noreply.github.com>
2023-02-09 19:07:14 +0530

    Update deleterobo.py

--- plugins/deleterobo.py
+++ plugins/deleterobo.py
@@ -13,23 +13,6 @@ import re
 client = MongoClient("mongodb+srv://Dipanshu_021:ad8920@cluster0.f7migc1.mongodb.net/?retryWrites=true&w=majority")
 db = client["mydatabase"]
 collection = db["group_timers"]
-
-@Client.on_message(filters.group)
-async def delete_after_time(client, message):
-    group_id = message.chat.id
-    print(message)
-    print(group_id)
-    # Check if there's a timer set for this group
-    timer = collection.find_one({"group_id": group_id})
-    
-    if timer:
-        # Calculate the time difference
-        now = time.time()
-        time_difference = now - message.date
-        
-        # If the time difference is greater than the timer, delete the message
-        if time_difference > timer["time"]:
-            await client.delete_messages(chat_id=group_id, message_ids=message.message_id)
 @Client.on_message(filters.command("set_timer"))
 async def set_timer(client, message):
     # Check if the user is an administrator
@@ -42,3 +25,19 @@ async def set_timer(client, message):
         
         # Confirm the timer has been set
         await client.send_message(chat_id=message.chat.id, text="Timer set successfully!")
+@Client.on_message(filters.group)
+async def delete_after_time(client, message):
+    group_id = message.chat.id
+    print(message)
+    print(group_id)
+    # Check if there's a timer set for this group
+    timer = collection.find_one({"group_id": group_id})
+    print(timer)
+    if timer:
+        # Calculate the time difference
+        now = time.time()
+        time_difference = now - message.date
+        
+        # If the time difference is greater than the timer, delete the message
+        if time_difference > timer["time"]:
+            await client.delete_messages(chat_id=group_id, message_ids=message.message_id)
